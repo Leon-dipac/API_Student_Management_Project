f917e5ee32e268f6b52a1dc64642835d71f9b868
Leon Dipac <leondipac@gmail.com>
2020-10-27 08:20:29 -0400

    Initial commit

---
+++ src/main/java/com/leondipac/ManagementApplication.java
@@ -0,0 +1,24 @@
+/* This tutorial is done with the help of a version control system known as Git.
+   Link: https://www.youtube.com/watch?v=uUzRMOCBorg
+
+ Project Link :
+ 	https://techblogstation.com/spring-boot/spring-boot-mysql-hibernate/
+
+ Test your front-end against a real API can be done using a hosted REST-API
+ 	ready to respond to your AJAX requests : https://reqres.in/
+
+*/
+
+package com.leondipac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leondipac/ManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leondipac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
