9dfdea7d8c4184b79b34c01343ba44d28b328f3f
Leon Dipac <leondipac@gmail.com>
2020-10-27 08:52:15 -0400

    Creation of the Object Class Model or StudentEntity Java Class

--- src/main/java/com/leondipac/ManagementApplication.java
+++ src/main/java/com/leondipac/ManagementApplication.java
@@ -18,6 +18,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ManagementApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ManagementApplication.class, args);
 	}
 
